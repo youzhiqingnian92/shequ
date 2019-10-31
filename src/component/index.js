45ab280f664a5dcc83556f02cea932b603c749a5
song <11@qq.com>
2019-10-31 17:17:28 +0800

    first commit

---
+++ src/component/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Comclass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          11112323
+        </h1>
+        <h2>login</h2>
+      </div>
+    );
+  }
+}
+export default Comclass;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React,{Component} from 'react';
+//引入路由
+import {HashRouter} from 'react-router-dom'
+
 import ReactDOM from 'react-dom';
 import './index.css';
+//引入antd移动框架
+import 'antd-mobile/dist/antd-mobile.css';
+//重置样式
+import './assets/css/reset.css'
+//适配移动端
+import './assets/js/rem'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//引入axios
+import axios from 'axios'
+Component.prototype.$axios= axios
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
